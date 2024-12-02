cfaa4216f4a2ab27b692bc169b20b9fcddaa7ccb
Elcid-Chawa <elcidchawa@gmail.com>
2024-12-02 15:01:19 +0200

    added learning to replace search

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -46,7 +46,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="search"
+        name="learn"
         options={{
           title: "Learn",
           tabBarIcon: ({ color }) => (
