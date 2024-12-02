2d5bb47ca35ab029264d77127967f03eb64dfb86
Elcid-Chawa <elcidchawa@gmail.com>
2024-12-02 15:02:34 +0200

    added learning and removed search

--- app/(tabs)/learn.tsx
+++ app/(tabs)/learn.tsx
@@ -12,22 +12,22 @@ import { ThemedView } from "@/components/ThemedView";
 import { useAuth } from "@/providers/AuthProvider";
 import { FlatList } from "react-native-gesture-handler";
 import { sampleFeaturedCourses } from "@/data/courses";
-import { useRouter} from "expo-router";
+import { useRouter } from "expo-router";
+import LearnCourse from "@/components/courses/LearnCourse";
 
-export default function SearchScreen() {
-  const { user } = useAuth();
+export default function LearnScreen() {
+  const { user, authUserData } = useAuth();
+  
   const router = useRouter();
   return (
     <SafeAreaView className="flex-1 bg-gndarkblue">
       {user ? (
         <FlatList
-          ListHeaderComponent={() => <ThemedText>Search</ThemedText>}
-          data={sampleFeaturedCourses}
+          ListHeaderComponent={() => <ThemedText>Learn</ThemedText>}
+          data={authUserData?.courses ?? []}
           renderItem={({ item }) =>
             user && (
-              <ThemedView>
-                <ThemedText>{item.name}</ThemedText>
-              </ThemedView>
+                <LearnCourse course={item} />
             )
           }
         />
@@ -36,7 +36,12 @@ export default function SearchScreen() {
           <Text className="text-white text-2xl">
             Login to continue Learning.
           </Text>
-          <TouchableOpacity className="mt-10 border border-gnLightBlue rounded-md p-2" onPress={() => {router.push("/sign-in")}}>
+          <TouchableOpacity
+            className="mt-10 border border-gnLightBlue rounded-md p-2"
+            onPress={() => {
+              router.push("/sign-in");
+            }}
+          >
             <Text className="text-white text-2xl">Login</Text>
           </TouchableOpacity>
         </View>
